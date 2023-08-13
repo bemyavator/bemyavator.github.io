cd16d36e88cc457634414fa59b254984f6f476f1
ju3tin <ju3tin@hotmail.co.uk>
2023-08-13 22:56:28 +0000

    ,m

--- js/main3a.js
+++ js/main3a.js
@@ -271,7 +271,7 @@ async function mintNFT1() {
   const price2 = await contract.methods.ACPrice3a().call();
   const price3 = await contract.methods.ACPrice3b().call();
   const price4 = await contract.methods.ACPrice3c().call();
-  if (pretest == true || pretest1 == true){const cost = price4};
+  if (pretest == true || pretest1 == true){price4 = cost};
   if (pretest == true || pretest1 == false){price3 = cost};
   if (pretest == false || pretest1 == false){price1 = cost};
   if (pretest == false || pretest1 == true){price2 = cost};
